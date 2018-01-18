94be98efb40596d3296d6b4b35f0a033d5a4d95a
Juan Carlos Sánchez G <jcarlos0305@hotmail.com>
2018-01-18 10:16:23 -0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+//TODO APP
+document.addEventListener('DOMContentLoaded', function () {
+
+  // select elements from the DOM
+  var todoList = document.getElementById('todoList');
+  var addTodo = document.getElementById('addTodo');
+  var todoVal = document.getElementById('todoVal');
+  var doneList = document.getElementById('doneList');
+
+  //Arrays
+  var todoArray = [];
+  var doneArray = [];
+
+  // add event handler to button
+  addTodo.addEventListener('click', addTodoToDOM);
+
+  function Action(name) {
+    this.name = name;
+    this.status = 'pending';
+  }
+
+  function updateLists() {
+    todoArray.map((action) => {
+      
+      var deleteButt = document.createElement('input');
+      deleteButt.type = 'checkbox';
+      let tempLi = document.createElement('li');
+      deleteButt.addEventListener('click', swapArrays);
+    });
+    todoList.append(tempList);
+  }
+
+  function swapArrays() {
+    console.log(this);
+    if (this.type == 'checkbox') {
+      this.type = 'button';
+      this.value = 'Done';
+      doneList.appendChild(this.parentNode);
+    } else {
+      this.type = 'checkbox';
+      this.checked = false;
+      todoList.appendChild(this.parentNode);
+    }
+  };
+
+  function addItem(){
+
+    var deleteButt = document.createElement('input');
+    deleteButt.type = 'checkbox';
+    deleteButt.addEventListener('click', swapArrays);
+
+    var todoText = document.createTextNode(todoArray[todoArray.length-1].name);
+    var todoListItem = document.createElement('li');
+    todoListItem.appendChild(todoText);
+    todoListItem.appendChild(deleteButt);
+    todoList.appendChild(todoListItem);
+  }
+
+  function addTodoToDOM(ev) {
+    todoArray.push(new Action(todoVal.value));
+    addItem();
+    todoVal.value = '';
+  }
+
+});
